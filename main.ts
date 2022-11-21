7308b01ebee9ad8df4d7279436b6b49fdabbaedd
TG144 <116873980+TG144@users.noreply.github.com>
2022-11-21 09:02:28 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Sprite_received = game.createSprite(receivedNumber % 5, Math.floor(receivedNumber / 5))
+})
 input.onButtonPressed(Button.A, function () {
-	
+    Pixel_1 = game.createSprite(Sprite.get(LedSpriteProperty.X), Sprite.get(LedSpriteProperty.Y))
+    radio.sendNumber(Pixel_1.get(LedSpriteProperty.Y) * 5 + Pixel_1.get(LedSpriteProperty.X))
 })
 input.onButtonPressed(Button.B, function () {
     if (Sprite.get(LedSpriteProperty.X) == 4 && Sprite.get(LedSpriteProperty.Y) == 4) {
@@ -12,9 +16,10 @@ input.onButtonPressed(Button.B, function () {
         Sprite.change(LedSpriteProperty.X, 1)
     }
 })
+let Pixel_1: game.LedSprite = null
+let Sprite_received: game.LedSprite = null
 let Sprite: game.LedSprite = null
+radio.setGroup(1)
+let Pixel = 0
 Sprite = game.createSprite(0, 0)
 Sprite.set(LedSpriteProperty.Brightness, 100)
-basic.forever(function () {
-	
-})
